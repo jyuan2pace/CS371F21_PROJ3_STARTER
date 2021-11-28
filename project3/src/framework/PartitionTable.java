aa7aa5df4873b42f77450a8b1033c5255aa711b0
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-11-28 11:12:58 -0500

    Update PartitionTable.java

--- project3/src/framework/PartitionTable.java
+++ project3/src/framework/PartitionTable.java
@@ -6,8 +6,8 @@ public class PartitionTable {
 	// (1) each partition works like a bounded buffer between
 	// mappers and a reducer. (you can assume size = 10 or 50)
 	// (2) if reducer_i wants to fetch a KV pair it can
-	// only fetches from partition_i, and mapper_i works in a
-        // similar manner
+	// only fetches from partition_i, but mapper_i can drop messages
+	// into different partitions.
     
 
 }
