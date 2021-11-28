516cb241505a5ae45aa54db14269392f875ea951
jun <jyuan2@pace.edu>
2021-11-28 11:34:52 -0500

    typo

--- project3/src/framework/MyMapReduce.java
+++ project3/src/framework/MyMapReduce.java
@@ -1,16 +1,16 @@
 package framework;
 import java.util.logging.Level;
 public class MyMapReduce extends MapReduce {
-	////TODO: your code here.
-	//	What is in a running instance of MapReduce?
+	//TODO: your code here. Define all attributes 
+	//What is in a running instance of MapReduce?
 	public void MREmit(Object key, Object value)
 	{
-		//TODO: your code here. Delete UnsupportedOperationException after your implemeantion is done.
+		//TODO: your code here. Delete UnsupportedOperationException after your implementation is done.
 		throw new UnsupportedOperationException();
 	}
 
 	public Object MRGetNext(Object key, int partition_number) {
-		//TODO: your code here. Delete UnsupportedOperationException after your implemeantion is done.
+		//TODO: your code here. Delete UnsupportedOperationException after your implementation is done.
 		throw new UnsupportedOperationException();
 	}
 	@Override
@@ -19,7 +19,7 @@ public class MyMapReduce extends MapReduce {
 		    		  int num_mappers, 
 		    		  int num_reducers)
 	{
-		//TODO: your code here. Delete UnsupportedOperationException after your implemeantion is done.
+		//TODO: your code here. Delete UnsupportedOperationException after your implementation is done.
 		throw new UnsupportedOperationException();
 	}
 }
